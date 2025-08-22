a812820f9dd064bf9e9c4ffb9ed6289f10d4e717
Paul Fontes <pgfon22@gmail.com>
2025-08-22 12:35:20 -0600

    Pickaxe is working

--- app.js
+++ app.js
@@ -1,8 +1,62 @@
-let cheese = 0;
+let cheese = 1000;
 
-function mine (){
-    cheese += 1
-    // alert(cheese)
+
+ let totalCheeseElm = document.getElementById('total-cheese')
+ let totalPickAxes = document.getElementById('total-pickaxes')
+
+ let clickUpgrades = [
+    {
+        name: 'pickaxe',
+        price: 100,
+        quantity: 1, 
+        bonus: 1
+    }
+ ]
+
+ let automaticUpgrades = [
+    {
+        name: 'rover',
+        price: 600,
+        quantity: 1,
+        bonus: 20
+    }
+ ]
+
+ function buyPickaxe() {
+     if(cheese > 100){
+     clickUpgrades.find((pickaxe) => pickaxe.quantity += 1)
+     clickUpgrades.find((pickaxe) => pickaxe.bonus = pickaxe.quantity)
+     clickUpgrades.find((price) => cheese -= price.price)
+    drawTotalCheese ()
+    console.log('purchased')
+    totalPickAxes.innerHTML = `Total Pickaxes: ${clickUpgrades[0].quantity}`
+    }
+ }
+
+function mine(){
+    clickUpgrades.find ((upgrade) => cheese += upgrade.bonus )
+    // cheese += 1
+    // console.log(cheese)
+    updateCheese ()
+    
 } 
 
-mine()
+function updateCheese () {
+    let totalCheese = 0
+    totalCheese = cheese
+    console.log('🧀', totalCheese)
+    drawTotalCheese ()
+}
+
+function drawTotalCheese () {
+    totalCheeseElm.innerHTML = `Total Cheese: ${cheese}`
+}
+
+// function drawPickAxe() {
+//     totalPickAxes.innerHTML = `Total Pickaxes: ${}`
+// }
+
+
+
+// mine()
+
